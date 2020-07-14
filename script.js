b94dcb898edca1aea45f4b5b2837d32b330c06f6
Sthavir <sthavmurthy@gmail.com>
2020-07-14 18:34:20 +0530

    All buttons wired

--- script.js
+++ script.js
@@ -39,6 +39,8 @@ function AC() {
     operand1 = null;
     operand2 = null;
     operator = null;
+    currentOp = 1;
+    opSwitched = false;
     displayCalculator();
 }
 function erase() {
@@ -52,10 +54,120 @@ function erase() {
     displayCalculator();
 }
 function fillNumber(number) {
-    if (displayNumber === "0" || displayNumber === NaN){
+    if (opSwitched) {
         displayNumber = number.toString();
+        opSwitched = false;
     } else {
-        displayNumber = displayNumber.concat(number);
+        if (displayNumber === "0" || displayNumber === "Not a number"){
+            displayNumber = number.toString();
+        } else {
+            displayNumber = displayNumber.concat(number);
+        }
+    }
+    displayCalculator();
+}
+function fillDecimal() {
+    if (displayNumber.indexOf("\.") === -1) {
+        fillNumber(decimalButton.innerHTML);
+    } else {
+        return;
+    }
+}
+
+function changeSign() {
+    if (displayNumber === "0" || displayNumber === NaN){
+        return;
+    } else {
+        if (displayNumber.indexOf("\-") === 0) {
+            displayNumber = displayNumber.substr(1);
+        } else {
+            displayNumber = "\-" + displayNumber;
+        }
+        displayCalculator();
+    }
+}
+function switchOp() {
+    if (currentOp === 1) {
+        currentOp = 2;
+    } else {
+        currentOp = 1;
+    }
+    opSwitched = true;
+}
+function sqrt() {
+    if ((displayNumber === "Not a number") || (Number(displayNumber) < 0)) {
+        displayNumber = "Not a number";
+    } else {
+        let sqrtResult = operate("sqrt", Number(displayNumber));
+        displayNumber = sqrtResult;
+    }
+    displayCalculator();
+}
+function square() {
+    if (displayNumber === "Not a number") {
+        displayNumber = "Not a number";
+    } else {
+        let squareResult = operate("square", Number(displayNumber));
+        displayNumber = squareResult;
+    }
+    displayCalculator();
+}
+function percentage() {
+    if (displayNumber === "Not a number") {
+        displayNumber = "Not a number";
+    } else {
+        let percentResult = operate("percent", Number(displayNumber));
+        displayNumber = percentResult;
+    }
+    displayCalculator();
+}
+function mult100() {
+    if (displayNumber === "Not a number") {
+        displayNumber = "Not a number";
+    } else {
+        let mult100Result = operate("mult100", Number(displayNumber));
+        displayNumber = mult100Result;
+    }
+    displayCalculator();
+}
+function dealWithOperands(pressedOperator) {
+    switch (currentOp) {
+        case 1:
+            operand1 = Number(displayNumber);
+            operator = pressedOperator;
+            switchOp();
+            break;
+        case 2:
+            evaluate();
+            operator = pressedOperator;
+            break;
+    }
+}
+function division() {
+    dealWithOperands("divide");
+}
+function multiplication() {
+    dealWithOperands("multiply");
+}
+function subtraction() {
+    dealWithOperands("subtract");
+}
+function addition() {
+    dealWithOperands("add");
+}
+function evaluate() {
+    if (currentOp = 2) {
+        operand2 = Number(displayNumber);
+    }
+    if (operand1 !== null && operand2 !== null && operator !== null) {
+        let evalResult = operate(operator, operand1, operand2);
+        displayNumber = evalResult.toString();
+        operand1 = evalResult;
+        operand2 = null;
+        operator = null;
+        opSwitched = true;
+    } else {
+        return;
     }
     displayCalculator();
 }
@@ -68,6 +180,8 @@ let displayNumber = "0";
 let operand1 = null;
 let operand2 = null;
 let operator = null;
+let currentOp = 1;
+let opSwitched = false;
 
 //Query Selectors
 const calcDisplay = document.querySelector("#display-input");
@@ -79,10 +193,11 @@ const multiplyButton = document.querySelector("#multiply");
 const subtractButton = document.querySelector("#subtract");
 const addButton = document.querySelector("#add");
 const squareButton = document.querySelector("#square");
-const changeSignButton = document.querySelector("chg-sign");
+const changeSignButton = document.querySelector("#chg-sign");
 const percentButton = document.querySelector("#percent");
 const mult100Button = document.querySelector("#mult100");
 const equalsButton = document.querySelector("#equals");
+const decimalButton = document.querySelector("#decimal");
 const numberButtons = document.querySelectorAll(".num-button");
 
 //Adding event handlers
@@ -91,7 +206,18 @@ eraseButton.addEventListener("click", () => erase());
 numberButtons.forEach((button) => {
     button.addEventListener("click", () => fillNumber(button.innerHTML));
 });
+decimalButton.addEventListener("click", () => fillDecimal());
+changeSignButton.addEventListener("click", () => changeSign());
+sqrtButton.addEventListener("click", () => sqrt());
+divideButton.addEventListener("click", () => division());
+multiplyButton.addEventListener("click", () => multiplication());
+subtractButton.addEventListener("click", () => subtraction());
+addButton.addEventListener("click", () => addition());
+squareButton.addEventListener("click", () => square());
+percentButton.addEventListener("click", () => percentage());
+mult100Button.addEventListener("click", () => mult100());
 
+equalsButton.addEventListener("click", () => evaluate());
 
 //Initialize
 displayCalculator();
